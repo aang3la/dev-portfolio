fce6ae1ea1cbe4e4f2510a9796435f51c4b4deee
Angela Antova <antovaangela@gmail.com>
2025-01-20 11:05:02 +0100

    flexible btn component

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -1,9 +1,18 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+    import { Button } from '$components';
+
+    function onclick() {
+       goto("/#contact-form");
+    }
+</script>
+
 <section class="hero-section default-margin">
     <h3>Hello! I'm Angela</h3>
     <div class="underscore mb-m"></div>
     <h1>I'm a Frontend Developer crafting intuitive and perfomant</h1>
     <h1 class="dark-grey">web experiences.</h1>
-    <button>Let's talk!</button>
+    <Button {onclick}>Let's talk!</Button>
 </section>
 
 <style>
